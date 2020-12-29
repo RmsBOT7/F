a62faa695cfa70603c96466a20b616a96582e037
Sunda XPloit Indo <75308262+RmsBOT7@users.noreply.github.com>
2020-12-29 08:13:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Halo Lord @${num.split('@')[0]}\nSelamat Datatang Di Group Kami Semoga Betah Ya Bro Jika Belum Save Nomer Admin Silahkan Save Dengan Ketik $creator *${mdata.subject}*`
+				teks = `Halo Lord @${num.split('@')[0]}\nSelamat Datang Di Group *${mdata.subject}* Semoga Betah Ya Bro Jika Belum Save Nomer Admin Silahkan Save Dengan Ketik $creator Powered :*${mdata.subject}*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
