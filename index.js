f240bb0661962c3c857e078a726b0e383195eae9
Sunda XPloit Indo <75308262+RmsBOT7@users.noreply.github.com>
2021-01-01 14:50:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1471,7 +1471,7 @@ async function starts() {
 				case 'ytmp4':
 					if (args.length < 1) return reply('Urlnya mana um?')
 					if(!isUrl(args[0]) && !args[0].includes('youtu')) return reply(mess.error.Iv)
-					anu = await fetchJson(`https://mhankbarbar.herokuapp.com/api/ytv?url=${args[0]}&apiKey=B4w56Fy3WQnfEyUNvQy8`, {method: 'get'})
+					anu = await fetchJson(`https://mhankbarbars.herokuapp.com/api/ytv?url=${args[0]}&apiKey=B4w56Fy3WQnfEyUNvQy8`, {method: 'get'})
 					if (anu.error) return reply(anu.error)
 					teks = `*Title* : ${anu.title}`
 					thumb = await getBuffer(anu.thumb)
